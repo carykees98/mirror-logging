4f12236f43482c1e97b5b9c5bede660e87b123d4
Juno <jvmeifert@gmail.com>
2023-10-24 14:30:18 -0400

    Implement setComponentName and sendLine
    Component name setup is now a separate function
    All functions now call sendLine which adds a routing ID to each message

--- src/logger.cpp
+++ src/logger.cpp
@@ -15,36 +15,39 @@ namespace mirror {
         // Initialize connection to log server
         m_LogServerSocket = zmq::socket_t(m_SocketContext, zmq::socket_type::stream);
         m_LogServerSocket.connect("tcp://localhost:" + std::to_string(port));
-
-        // Send log server the component name
-        std::string componentNameMessage = "@ComponentName " + componentName + "\r\n";
-        m_LogServerSocket.send(zmq::message_t(componentNameMessage), zmq::send_flags::none);
-
         m_Configured = true;
+        
+        // Send log server the component name
+        setComponentName(componentName);
     }
 
-    void Logger::sendLogMessage(const std::string &logMessage) {
+    void Logger::sendLine(const std::string &line) {
         // if not configured throw exception, will likely end program unless wrapped in try catch
         if (!m_Configured) { throw std::logic_error("Error: Logger Not Configured"); }
 
         // send message
-        m_LogServerSocket.send(zmq::message_t(logMessage + "\r\n"), zmq::send_flags::none);
+        std::string routingID = m_LogServerSocket.get(zmq::sockopt::routing_id);
+        m_LogServerSocket.send(zmq::message_t(routingID), zmq::send_flags::sndmore);
+        m_LogServerSocket.send(zmq::message_t(line + "\r\n"), zmq::send_flags::none);
     }
 
     [[maybe_unused]] void Logger::info(const std::string &logMessage) {
-        sendLogMessage(std::to_string((int) LogLevels::Info) + logMessage);
+        sendLine(std::to_string((int) LogLevels::Info) + logMessage);
     }
 
     [[maybe_unused]] void Logger::warn(const std::string &logMessage) {
-        sendLogMessage(std::to_string((int) LogLevels::Warn) + logMessage);
+        sendLine(std::to_string((int) LogLevels::Warn) + logMessage);
     }
 
     [[maybe_unused]] void Logger::error(const std::string &logMessage) {
-        sendLogMessage(std::to_string((int)LogLevels::Error) + logMessage);
+        sendLine(std::to_string((int)LogLevels::Error) + logMessage);
     }
 
     [[maybe_unused]] void Logger::fatal(const std::string &logMessage) {
-        sendLogMessage(std::to_string((int) LogLevels::Fatal) + logMessage);
+        sendLine(std::to_string((int) LogLevels::Fatal) + logMessage);
+    }
+
+    [[maybe_unused]] void Logger::setComponentName(const std::string &componentName) {
+        sendLine("@ComponentName " + componentName);
     }
 }
-
