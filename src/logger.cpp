a65e8770440c86ea85f04989094a0c9480d257d1
Juno <jvmeifert@gmail.com>
2024-03-01 09:27:55 -0500

    Add local logging, add debug level

--- src/logger.cpp
+++ src/logger.cpp
@@ -5,6 +5,8 @@
 #include <mutex>
 #include <string>
 #include <thread>
+#include <iomanip>
+#include <iostream>
 
 // Library includes
 #include <zmq.hpp>
@@ -32,24 +34,32 @@ namespace mirror {
         return s_Instance;
     }
 
+    [[maybe_unused]] void Logger::debug(const std::string &logMessage) {
+        printEvent(" \u001B[34m[ DEBUG ]\u001B[0m ", logMessage);
+    }
+
     [[maybe_unused]] void Logger::info(const std::string &logMessage) {
         std::string lineToSend = "@" + std::to_string((int) LogLevels::Info) + logMessage;
         f_SendLine(lineToSend);
+        printEvent(" \u001B[32m[ INFO ]\u001B[0m  ", logMessage);
     }
 
     [[maybe_unused]] void Logger::warn(const std::string &logMessage) {
         std::string lineToSend = "@" + std::to_string((int) LogLevels::Warn) + logMessage;
         f_SendLine(lineToSend);
+        printEvent(" \u001B[33m[ WARN ]\u001B[0m  ", logMessage);
     }
 
     [[maybe_unused]] void Logger::error(const std::string &logMessage) {
         std::string lineToSend = "@" + std::to_string((int) LogLevels::Error) + logMessage;
         f_SendLine(lineToSend);
+        printEvent(" \u001B[31m[ ERROR ]\u001B[0m ", logMessage);
     }
 
     [[maybe_unused]] void Logger::fatal(const std::string &logMessage) {
         std::string lineToSend = "@" + std::to_string((int) LogLevels::Fatal) + logMessage;
         f_SendLine(lineToSend);
+        printEvent(" \u001B[31m[ FATAL ]\u001B[0m ", logMessage);
     }
 
     void Logger::configure(uint16_t port, const std::string &componentName, const std::string &address) {
@@ -57,6 +67,7 @@ namespace mirror {
 
         m_URL = "tcp://" + address + ":" + std::to_string(port);
         m_LogServerSocket.connect(m_URL);
+        
 
         m_ComponentName = componentName;
         m_Configured = true;
@@ -81,4 +92,11 @@ namespace mirror {
         m_LogServerSocket.send(message, zmq::send_flags::none);
     }
 
+    void Logger::printEvent(const char* level_tag, const std::string &message) {
+        std::time_t now = std::time(nullptr);
+        auto time = *std::localtime(&now);
+        std::cout << std::put_time(&time, "%Y-%m-%d %H:%M:%S");
+        std::cout << level_tag << std::left << std::setw(20);
+        std::cout << m_ComponentName << " : " << message << std::endl;
+    }
 } // namespace mirror
